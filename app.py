62170484ddf5f2207fd9321f1022d08fed46501f
gazala ansari <55708169+gazala-ansari@users.noreply.github.com>
2021-05-07 03:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def get_bot_response():
     userText = request.args.get('msg')
     res=try1(userText)
     return str((res)) 
-
-	
-    
+if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
